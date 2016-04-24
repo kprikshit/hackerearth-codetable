67c6ac93f9e4ca053ef657d773d978c36be0ba2b
Prikshit Kumar <prikshit.kumar@blobcity.com>
2016-04-25 01:40:12 +0530

    Everything working except compile and run

--- editor/editorViews.py
+++ editor/editorViews.py
@@ -1,70 +1,169 @@
-from django.shortcuts import render
 from django.http import *
-from django.template import Context, loader
-from django.shortcuts import redirect
+from django.template import loader
 from editor.models import Code
-from django.utils import timezone
 from codetable.randomFileUrl import generateRandomString
 from languageExtension import *
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Database related function
 def check_in_database(code_id):
-	return Code.objects.filter(pk=code_id).exists()
+    return Code.objects.filter(pk=code_id).exists()
+
 
 def insert_into_database(code_id):
-	Code.addDefault(Code(), code_id);
+    Code.addDefault(Code(), code_id)
+
 
 def get_from_database(code_id):
-	if(check_in_database(code_id)):
-		return Code.objects.get(pk=code_id)
-	else:
-		return null;
+    if check_in_database(code_id):
+        return Code.objects.get(pk=code_id)
+    else:
+        return
+
+@csrf_exempt
+def generate(request):
+    code_id = ""
+    if request.method == "POST":
+        code_id = request.POST.get('code_id', '')
+    else:
+        code_id = request.GET.get('code_id', '')
+    if code_id == "":
+        return HttpResponse("");
+
+    obj = get_from_database(code_id)
+    obj.shared = True
+    obj.share_url = code_id
+    obj.save();
+
+    share_url = get_share_url(request.get_host(), code_id);
+    return  HttpResponse(share_url)
+
+
+def get_share_url(host, code_id):
+    return "http://" + host + "/editor/" + code_id +"/"
 
 '''
 redirect function for /editor/ to /editor/randomString/
 '''
 def editor_blank_redirect(request):
-    final_url = generateRandomString();
-    return HttpResponseRedirect(final_url);
+    final_url = generateRandomString()
+    return HttpResponseRedirect(final_url)
+
+
+def get_code_text(code_id, code_lang):
+    file_name = generateFileName(code_id, code_lang)
+    code_obj = open("codes/"+file_name, "r")
+    return code_obj.read()
+
+'''
+Get the starter code for various language
+'''
+def get_default_code_text(code_lang):
+    if code_lang == DEFAULT_C_NAME:
+        return C_STARTER_CODE
+    elif code_lang == DEFAULT_CPP_NAME:
+        return CPP_STARTER_CODE
+    elif code_lang == DEFAULT_CPP11_NAME:
+        return CPP11_STARTER_CODE
+    elif code_lang == DEFAULT_JAVA_NAME:
+        return JAVA_STARTER_CODE
+    elif code_lang == DEFAULT_JAVASCRIPT_NAME:
+        return JAVASCRIPT_STARTER_CODE
+    elif code_lang == DEFAULT_PERL_NAME:
+        return PERL_STARTER_CODE
+    elif code_lang == DEFAULT_PHP_NAME:
+        return PHP_STARTER_CODE
+    elif code_lang == DEFAULT_PYTHON_NAME:
+        return PYTHON_STARTER_CODE
+    elif code_lang == DEFAULT_RUBY_NAME:
+        return RUBY_STARTER_CODE
+    elif code_lang == DEFAULT_TEXT_NAME:
+        return TEXT_STARTER_CODE
+    else:
+        return TEXT_STARTER_CODE
+
+
+def get_default_lang_extension(code_lang):
+    if code_lang == DEFAULT_C_NAME:
+        return C_EXTENSION
+    elif code_lang == DEFAULT_CPP_NAME or code_lang == DEFAULT_CPP11_NAME:
+        return CPP_EXTENSION
+    elif code_lang == DEFAULT_JAVA_NAME:
+        return JAVA_EXTENSION
+    elif code_lang == DEFAULT_JAVASCRIPT_NAME:
+        return JAVASCRIPT_EXTENSION
+    elif code_lang == DEFAULT_PERL_NAME:
+        return PERL_EXTENSION
+    elif code_lang == DEFAULT_PHP_NAME:
+        return PHP_EXTENSION
+    elif code_lang == DEFAULT_PYTHON_NAME:
+        return PYTHON_EXTENSION
+    elif code_lang == DEFAULT_RUBY_NAME:
+        return RUBY_EXTENSION
+    elif code_lang == DEFAULT_TEXT_NAME:
+        return TEXT_EXTENSION
+    else:
+        return TEXT_EXTENSION
+
+@csrf_exempt
+def get_starter_code(request):
+    code_lang = request.POST.get('lang', '')
+    if code_lang =="":
+        return HttpResponse("")
+    return HttpResponse(get_default_code_text(code_lang))
+
 
 def new_code(request, code_id):
-	template = loader.get_template('base.html')
-	check = check_in_database(code_id)
-	if(check):
-		print "already inserted"
-	else:
-		insert_into_database(code_id)
-	obj = get_from_database(code_id);
-	context = {'file_name': code_id, 'code_modify_date': obj.last_save_date, 'code_create_date': obj.create_date}
-	return HttpResponse(template.render(context, request))	
+    template = loader.get_template('base.html')
+    presentInDb = check_in_database(code_id)
+    code_text = "";
+    code_lang = DEFAULT_LANGUAGE;
 
-def generateFileName(name, lang):
-	fileName = "";
-	if(lang=="C"):
-		fileName = name + C_EXTENSION
-	elif(lang == "CPP" or lang == "CPP11"):
-		fileName = name + CPP_EXTENSION
-	elif(lang=="JAVA"):
-		fileName = name + JAVA_EXTENSION
-	elif(lang=="JAVASCRIPT"):
-		fileName = name + JAVASCRIPT_EXTENSION
-	elif(lang=="PERL"):
-		fileName = name + PERL_EXTENSION
-	elif(lang=="PHP"):
-		fileName = name + PHP_EXTENSION
-	elif(lang=="PYTHON"):
-		fileName = name + PYTHON_EXTENSION
-	elif(lang=="RUBY"):
-		fileName = name + RUBY_EXTENSION
-	else:
-		fileName = name + TEXT_EXTENSION
-	return fileName;
+    #if the code was already present in database
+    if presentInDb:
+        obj2 = get_from_database(code_id)
+        code_lang = obj2.code_lang
+        code_text = get_code_text(code_id, code_lang)
+    # if the code is not present in database
+    else:
+        insert_into_database(code_id)
+        code_text = DEFAULT_TEXT
+        code_lang = DEFAULT_LANGUAGE
+    obj = get_from_database(code_id)
+    context = {'file_name': code_id, 'code_last_save_date': obj.last_save_date, 'code_create_date': obj.create_date, 'code_text': code_text, 'code_lang': code_lang, 'shared': False}
+    if obj.shared:
+        context['shared_url'] = get_share_url(request.get_host(), code_id)
+        context['shared'] = True
+    return HttpResponse(template.render(context, request))
 
 
+def generateFileName(file_name, code_lang):
+    return file_name + get_default_lang_extension(code_lang);
 
+
+def update_in_database(code_id, curr_date, code_lang):
+    code_obj = Code.objects.get(pk=code_id)
+    code_obj.last_save_date = curr_date
+    code_obj.code_lang = code_lang
+    code_obj.save();
+
+@csrf_exempt
 def save(request):
-	print 'Lang: ' + request.POST.get('lang','');
-	print 'Code: ' + request.POST.get('code','');
-	print 'id: ' + request.POST.get('id','');
-	print generateFileName(request.POST.get('id',''), request.POST.get('lang',''))
-	return HttpResponse("received received");
+    code_lang = request.POST.get('lang', '')
+    code_text = request.POST.get('text', '')
+    code_id = request.POST.get('id', '')
+    try:
+        #save the code in the file
+        codeFile = open('codes/'+generateFileName(code_id, code_lang), 'w+')
+        codeFile.write(code_text);
+        codeFile.close();
+        save_time = timezone.now();
+        # update in database now
+        update_in_database(code_id, save_time, code_lang)
+        return HttpResponse(str(save_time) );
+    except Exception as ex:
+        print ex
+        error = '{"ack:0", "msg": "error in saving file"}'
+        return HttpResponse(error)
--- editor/urls.py
+++ editor/urls.py
@@ -4,5 +4,7 @@ from . import editorViews
 urlpatterns = [
     url(r'^$', editorViews.editor_blank_redirect, name='Blank'),
     url(r'^save/$', editorViews.save, name='Save'),
-    url(r'^(?P<code_id>\w+)/', editorViews.new_code, name='CodeId'),    
+    url(r'^generate/$', editorViews.generate, name='Save'),
+    url(r'^starter/$', editorViews.get_starter_code, name='CodeText'),
+    url(r'^(?P<code_id>\w+)/', editorViews.new_code, name='CodeId'),
 ]
